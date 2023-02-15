69be9451b8d32fa079f38f2fedac82006619d4c5
asime204 <azielsime1@gmail.com>
2023-02-15 11:43:11 -0500

    todo list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home';
-import Login from './Login';
-import Nav from './Nav'
+import Nav from './components/Nav';
+import ToDo from './views/ToDo';
 
 
 export default class App extends Component {
   constructor(){
-    super();
+    super();this.state = {
+      age: 9000,
+      myList: [],
+    }
     
   }
+  addToDo = (e) => {
+    e.preventDefault();
+    const text = e.target.myText.value
+    this.setState({ myList: this.state.myList.concat([text]) })
+  };
+  deleteToDo = (indexToDelete) => {
+    const copy = [...this.state.myList]
+    copy.splice(indexToDelete, 1)
+    this.setState({myList: copy})
+  };
   render() {
     return (
+      <Router>
       <div>
         <Nav />
-        <Home />
-        {/* <Login /> */}
+
+        <Routes>
+          <Route path='/' element={<Home age={this.state.age} happyBirthday={this.happyBirthday} />} />
+          <Route path='/todo' element={<ToDo myList={this.state.myList} handleToDoSubmit={this.addToDo} deleteToDo={this.deleteToDo}/>} />
+        </Routes>
+
       </div>
+    </Router>
     )
   }
 }
